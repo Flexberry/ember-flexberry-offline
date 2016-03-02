a92555f98209baa4a39b17bec8c1159547ab0f1a
Evgeniy Khaberev <ehaberev@yandex.ru>
2016-03-02 17:39:27 +0500

    Add 'offline-support' adapter

---
+++ addon/adapters/offline-support.js
@@ -0,0 +1,15 @@
+import DS from 'ember-data';
+
+/**
+@module ember-flexberry-offline
+**/
+
+/**
+ * Adapter that can switch between online and offline modes.
+ *
+ * @class OfflineSupportAdapter
+ * @extends DS.Adapter
+ * @public
+ */
+export default DS.Adapter.extend({
+});
---
+++ app/adapters/offline-support.js
@@ -0,0 +1,2 @@
+import OfflineSupportAdapter from 'ember-flexberry-offline/adapters/offline-support';
+export default OfflineSupportAdapter;
