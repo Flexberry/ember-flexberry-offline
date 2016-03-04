ce05cf15c7c82b49fadf85195318a736c217517b
Evgeniy Khaberev <ehaberev@yandex.ru>
2016-03-04 13:13:37 +0500

    Some correction of 'base' adapter

--- addon/adapters/base.js
+++ addon/adapters/base.js
@@ -1,13 +1,14 @@
 import DS from 'ember-data';
 
 /**
-@module ember-flexberry-offline
-**/
+ * @module ember-flexberry-offline
+ */
 
 /**
  * Adapter that can switch between online and offline modes.
  *
  * @class BaseAdapter
+ * @namespace Offline
  * @extends DS.Adapter
  * @public
  */
--- app/adapters/base.js
+++
@@ -1,2 +0,0 @@
-import BaseAdapter from 'ember-flexberry-offline/adapters/base';
-export default BaseAdapter;
