3413412ca28d198098254a9878d148922b50b0df
Evgeniy Khaberev <ehaberev@yandex.ru>
2016-04-29 12:09:07 +0500

    Fix `local-serializer`
    Remove `EmbeddedRecordsMixin` from `local-serializer`
    
    Remove `serializeBelongsTo` method from `local-serializer`
    It's redundant
    
    Fix extracting relationship in `local-serializer`

--- addon/serializers/local-serializer.js
+++ addon/serializers/local-serializer.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import LFSerializer from 'ember-localforage-adapter/serializers/localforage';
 import isObject from '../utils/is-object';
 
-export default LFSerializer.extend(DS.EmbeddedRecordsMixin, {
+export default LFSerializer.extend({
   init: function() {
     this._super(...arguments);
     let owner = Ember.getOwner(this);
@@ -10,36 +10,17 @@ export default LFSerializer.extend(DS.EmbeddedRecordsMixin, {
     this.set('store', localStore);
   },
 
-  /**
-   `serializeBelongsTo` can be used to customize how `DS.belongsTo` properties are serialized.
-   If there is set option `odata-id` at serializer and `DS.belongsTo` property is not null,
-   then property will be serialized like `ids`.
-
-   @method serializeBelongsTo
-   @param {DS.Snapshot} snapshot
-   @param {Object} json
-   @param {Object} relationship
-  */
-  serializeBelongsTo(snapshot, json, relationship) {
-    var option = this.attrsOption(relationship.key);
-    if (!option || option.serialize !== 'odata-id') {
-      this._super(snapshot, json, relationship);
-      return;
-    }
-
-    var key = relationship.key;
-    var belongsToId = snapshot.belongsTo(key, { id: true });
-    var payloadKey = this.keyForRelationship(key, relationship.kind, 'serialize');
-    if (Ember.isNone(belongsToId)) {
-      json[payloadKey] = null;
-    } else {
-      json[payloadKey] = belongsToId;
-    }
-  },
-
   extractRelationship (relationshipModelName, relationshipHash) {
     if (isObject(relationshipHash) && Ember.isNone(relationshipHash.type)) {
       relationshipHash.type = relationshipModelName;
+    }
+    else if (!isObject(relationshipHash) && !Ember.isNone(relationshipHash)) {
+      var hash = {
+        id: relationshipHash,
+        type: relationshipModelName
+      };
+
+      relationshipHash = hash;
     }
 
     return relationshipHash;
