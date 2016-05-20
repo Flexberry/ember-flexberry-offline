ccf1cf1fd84ff0c07f6c0918b63873b18ce1ec72
Evgeniy Khaberev <ehaberev@yandex.ru>
2016-05-20 21:02:51 +0500

    Add `useOnlineStore` option to `base-store` methods
    to explicitly specify online or offline store for passing control flow independently of
    global online status.
    Also reformatting code.

--- addon/stores/base-store.js
+++ addon/stores/base-store.js
@@ -45,11 +45,14 @@ export default DS.Store.extend({
     @method findAll
     @param {String} modelName
     @param {Object} options
+    @param {Boolean} [options.useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
     @return {Promise} promise
   */
-  findAll: function (modelName, options) {
+  findAll(modelName, options) {
     let offlineStore = this.get('offlineStore');
-    return this._isOnline() ? this._decorateMethodAndCall('all', 'findAll', arguments, 1) : offlineStore.findAll.apply(offlineStore, arguments);
+    let useOnlineStore = options ? options.useOnlineStore : null;
+    let useOnlineStoreCondition = (useOnlineStore === true) || (useOnlineStore === null && this._isOnline());
+    return useOnlineStoreCondition ? this._decorateMethodAndCall('all', 'findAll', arguments, 1) : offlineStore.findAll.apply(offlineStore, arguments);
   },
 
   /**
@@ -60,11 +63,14 @@ export default DS.Store.extend({
     @param {String} modelName
     @param {String|Integer} id
     @param {Object} options
+    @param {Boolean} [options.useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
     @return {Promise} promise
    */
-  findRecord: function(modelName, id, options) {
+  findRecord(modelName, id, options) {
     let offlineStore = this.get('offlineStore');
-    return this._isOnline() ? this._decorateMethodAndCall('single', 'findRecord', arguments, 2) : offlineStore.findRecord.apply(offlineStore, arguments);
+    let useOnlineStore = options ? options.useOnlineStore : null;
+    let useOnlineStoreCondition = (useOnlineStore === true) || (useOnlineStore === null && this._isOnline());
+    return useOnlineStoreCondition ? this._decorateMethodAndCall('single', 'findRecord', arguments, 2) : offlineStore.findRecord.apply(offlineStore, arguments);
   },
 
   /**
@@ -77,7 +83,7 @@ export default DS.Store.extend({
     @param {DS.Model} internalModel
     @return {Promise} promise
   */
-  reloadRecord: function (internalModel) {
+  reloadRecord() {
     let offlineStore = this.get('offlineStore');
     return this._isOnline() ? this._decorateMethodAndCall('single', 'reloadRecord', arguments, -1) : offlineStore.reloadRecord.apply(offlineStore, arguments);
   },
@@ -87,11 +93,17 @@ export default DS.Store.extend({
     @method query
     @param {String} modelName
     @param {Object} query
+    @param {Boolean} [query.useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
     @return {Promise} promise
   */
-  query: function (modelName, query) {
+  query(modelName, query) {
     let offlineStore = this.get('offlineStore');
-    return this._isOnline() ? this._decorateMethodAndCall('multiple', 'query', arguments, -1) : offlineStore.query.apply(offlineStore, arguments);
+    let useOnlineStore = query ? query.useOnlineStore : null;
+    if (query.useOnlineStore) {
+      delete query.useOnlineStore;
+    }
+    let useOnlineStoreCondition = (useOnlineStore === true) || (useOnlineStore === null && this._isOnline());
+    return useOnlineStoreCondition ? this._decorateMethodAndCall('multiple', 'query', arguments, -1) : offlineStore.query.apply(offlineStore, arguments);
   },
 
   /**
@@ -99,51 +111,226 @@ export default DS.Store.extend({
     @method query
     @param {String} modelName
     @param {Object} query
+    @param {Boolean} [query.useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
     @return {Promise} promise
   */
-  queryRecord: function (modelName, query) {
+  queryRecord(modelName, query) {
     let offlineStore = this.get('offlineStore');
-    return this._isOnline() ? this._decorateMethodAndCall('single', 'queryRecord', arguments, -1) : offlineStore.queryRecord.apply(offlineStore, arguments);
+    let useOnlineStore = query ? query.useOnlineStore : null;
+    if (query.useOnlineStore) {
+      delete query.useOnlineStore;
+    }
+    let useOnlineStoreCondition = (useOnlineStore === true) || (useOnlineStore === null && this._isOnline());
+    return useOnlineStoreCondition ? this._decorateMethodAndCall('single', 'queryRecord', arguments, -1) : offlineStore.queryRecord.apply(offlineStore, arguments);
   },
 
-  adapterFor: function(type) {
+  /**
+    Create a new record in the current store. The properties passed to this method are set on the newly created record.
+    @method createRecord
+    @param {String} modelName
+    @param {Object} inputProperties A hash of properties to set on the newly created record.
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+    @return {Promise} promise
+  */
+  createRecord() {
+    return this._callSuperMethod('createRecord', 2, arguments);
+  },
+
+  /**
+    For symmetry, a record can be deleted via the store.
+    @method deleteRecord
+    @param {DS.Model} record
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+  */
+  deleteRecord() {
+    return this._callSuperMethod('deleteRecord', 1, arguments);
+  },
+
+  /**
+    Get the reference for the specified record.
+    @method getReference
+    @param {String} type
+    @param {String|Integer} id
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+    @return {RecordReference} promise
+  */
+  getReference() {
+    return this._callSuperMethod('getReference', 2, arguments);
+  },
+
+  /**
+    Returns true if a record for a given type and ID is already loaded.
+    @method hasRecordForId
+    @param {String|DS.Model} modelName
+    @param {String|Integer} inputId
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+    @return {Boolean}
+  */
+  hasRecordForId() {
+    return this._callSuperMethod('hasRecordForId', 2, arguments);
+  },
+
+  /**
+    Converts a json payload into the normalized form that {{#crossLink "BaseStore/push:method"}}{{/crossLink}} expects.
+    @method normalize
+    @param {String} modelName The name of the model type for this payload
+    @param {Object} payload
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+    @return {Object} The normalized payload
+  */
+  normalize() {
+    return this._callSuperMethod('normalize', 2, arguments);
+  },
+
+  /**
+    This method returns a filtered array that contains all of the known records for a given type in the store.
+    @method peekAll
+    @param {String} modelName
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+    @return {DS.RecordArray}
+  */
+  peekAll() {
+    return this._callSuperMethod('peekAll', 1, arguments);
+  },
+
+  /**
+    Get a record by a given type and ID without triggering a fetch.
+    @method peekRecord
+    @param {String} modelName
+    @param {String|Integer} id
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+    @return {DS.Model|null} Record
+  */
+  peekRecord() {
+    return this._callSuperMethod('peekRecord', 2, arguments);
+  },
+
+  /**
+    Push some data for a given type into the store.
+    @method push
+    @param {Object} data
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+    @return {DS.Model|Array} The record(s) that was created or updated.
+  */
+  push() {
+    return this._callSuperMethod('push', 1, arguments);
+  },
+
+  /**
+    Push some raw data into the store.
+    @method pushPayload
+    @param {String} modelName
+    @param {Object} inputPayload
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+  */
+  pushPayload() {
+    return this._callSuperMethod('pushPayload', 2, arguments);
+  },
+
+  /**
+    This method returns if a certain record is already loaded in the store.
+    @method recordIsLoaded
+    @param {String} modelName
+    @param {String|Integer} id
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+    @return {Boolean}
+  */
+  recordIsLoaded() {
+    return this._callSuperMethod('recordIsLoaded', 2, arguments);
+  },
+
+  /**
+    This method unloads all records in the store.
+    @method unloadAll
+    @param {String} modelName
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+  */
+  unloadAll() {
+    return this._callSuperMethod('unloadAll', 1, arguments);
+  },
+
+  /**
+    For symmetry, a record can be unloaded via the store. Only non-dirty records can be unloaded.
+    @method unloadRecord
+    @param {DS.Model} record
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+  */
+  unloadRecord() {
+    return this._callSuperMethod('unloadRecord', 1, arguments);
+  },
+
+  /**
+    Returns an instance of the adapter for a given type.
+    @method adapterFor
+    @param {String} modelName
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+    @return {DS.Adapter or subclass} Adapter
+  */
+  adapterFor(modelName, useOnlineStore) {
     let onlineStore = this.get('onlineStore');
     let offlineStore = this.get('offlineStore');
-    let adapter = onlineStore.adapterFor(type);
+    let adapter = onlineStore.adapterFor(modelName);
     if (this.get('offlineGlobals').get('isOfflineEnabled')) {
-      return this._isOnline() ? decorateAdapter.call(this, adapter) : offlineStore.adapterFor(type);
-	}
+      let useOnlineStoreCondition = useOnlineStore || (Ember.isNone(useOnlineStore) && this._isOnline());
+      return useOnlineStoreCondition ? decorateAdapter.call(this, adapter) : offlineStore.adapterFor(modelName);
+    }
     else {
       return adapter;
     }
   },
 
-  serializerFor: function(type) {
+  /**
+    Returns an instance of the serializer for a given type.
+    @method serializerFor
+    @param {String} modelName
+    @param {Boolean} [useOnlineStore] Allow to explicitly specify online or offline store using independently of global online status
+    @return {DS.Serializer or subclass} Serializer
+  */
+  serializerFor(modelName, useOnlineStore) {
     let onlineStore = this.get('onlineStore');
     let offlineStore = this.get('offlineStore');
-    let serializer = onlineStore.serializerFor(type);
+    let serializer = onlineStore.serializerFor(modelName);
     if (this.get('offlineGlobals').get('isOfflineEnabled')) {
-      return this._isOnline() ? decorateSerializer.call(this, serializer) : offlineStore.serializerFor(type);
-	}
+      let useOnlineStoreCondition = useOnlineStore || (Ember.isNone(useOnlineStore) && this._isOnline());
+      return useOnlineStoreCondition ? decorateSerializer.call(this, serializer) : offlineStore.serializerFor(modelName);
+    }
     else {
       return serializer;
     }
   },
 
-  _decorateMethodAndCall: function(finderType, originMethodName, originMethodArguments, optionsIndex) {
+  /*
+    Decorate specified method of online store for add extra syncing features.
+  */
+  _decorateMethodAndCall(finderType, originMethodName, originMethodArguments, optionsIndex) {
     let onlineStore = this.get('onlineStore');
     let originMethod = onlineStore[originMethodName];
     let decoratedMethod = decorateAPICall(finderType, originMethod);
-	if (optionsIndex > -1) {
+	  if (optionsIndex > -1) {
       let options = originMethodArguments[optionsIndex];
-	  options = this.get('offlineGlobals').get('isOfflineEnabled') ? options : Ember.$.extend(true, { bypass: true }, options);
+	    options = this.get('offlineGlobals').get('isOfflineEnabled') ? options : Ember.$.extend(true, { bypass: true }, options);
       originMethodArguments[optionsIndex] = options;
     }
 
     return decoratedMethod.apply(onlineStore, originMethodArguments);
   },
 
-  _isOnline: function() {
+  /*
+    Detect global online status.
+  */
+  _isOnline() {
     return this.get('offlineGlobals.isOnline');
+  },
+
+  /*
+    Pass control flow to online or offline store depend on global online status
+    and explicitly specified store to use.
+  */
+  _callSuperMethod(methodName, useOnlineStoreParamNum, ...args) {
+    let onlineStore = this.get('onlineStore');
+    let offlineStore = this.get('offlineStore');
+    let useOnlineStore = (args[0].length - 1) >= useOnlineStoreParamNum ? args[0][useOnlineStoreParamNum] : null;
+    let useOnlineStoreCondition = useOnlineStore || (Ember.isNone(useOnlineStore) && this._isOnline());
+    return useOnlineStoreCondition ? onlineStore[methodName].apply(onlineStore, args[0]) : offlineStore[methodName].apply(offlineStore, args[0]);
   }
 });
