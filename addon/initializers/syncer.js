e78f92680c92e0dcde41875741ea707ad1031172
Evgeniy Khaberev <ehaberev@yandex.ru>
2016-05-31 17:50:36 +0500

    Fix using of `store` initializer
    `store` initializer was added to keep backwards compatibility since
    ember-data 2.5.0

--- addon/initializers/syncer.js
+++ addon/initializers/syncer.js
@@ -1,11 +1,12 @@
 import Syncer from '../syncer';
 
 export function initialize(application) {
+  //Register factory for Syncer.
   application.register('syncer:main', Syncer);
 }
 
 export default {
   name: 'syncer',
-  before: 'store',
+  before: 'ember-data',
   initialize
 };
